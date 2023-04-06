4e3b5c94c952fbe719a8fd03732f48f8c671c054
Imran <huseynlimran@gmail.com>
2023-04-06 16:06:53 +0400

    data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { gsap } from "gsap";
+import "./App.css";
+import { BsTelegram, BsFacebook,BsWhatsapp } from "react-icons/bs";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+    gsap.fromTo(".icon",
+    {opacity:0,y:100},
+    {opacity:1,duration:1,y:0,stagger:.3});
+    gsap.fromTo(".title",
+    {opacity:0,y:-100},
+    {opacity:1,duration:1,y:0,stagger:.3})
+
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="socials">
+        <h1 className="title">Contact</h1>
+        <div className="social">
+        <a href="https://wa.me/994777500037" className="icon" target="_blank" rel="noreferrer">
+          <BsWhatsapp />
         </a>
-      </header>
+        <a href="https://t.me/stomatoloji_avadanliqlar" className="icon" target="_blank" rel="noreferrer">
+          <BsTelegram />
+        </a>
+        <a
+          href="https://www.facebook.com/groups/834791693924494/?ref=share&mibextid=NSMWBT"
+          target="_blank" className="icon" rel="noreferrer"
+        >
+          <BsFacebook />
+        </a>
+        </div>
+        
+      </div>
     </div>
   );
 }
